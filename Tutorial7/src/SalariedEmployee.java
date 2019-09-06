e6c1bc8881c6f6a21cd032e060edff72fff5522b
12095732 <12095732@192.168.56.1>
2019-09-06 15:40:07 +1000

    name-Tajinder Pal Singh Student-ID - 12095732 aa

--- Tutorial7/src/SalariedEmployee.java
+++ Tutorial7/src/SalariedEmployee.java
@@ -11,13 +11,46 @@
 public class SalariedEmployee implements Payable{
     
     double weeklySalary; 
+    String name;
+    String lastname;
+    String Abc;
+
+    public SalariedEmployee(String name, String lastname, String Abc,double weeklySalary) {
+        this.weeklySalary = weeklySalary;
+        this.name = name;
+        this.lastname = lastname;
+        this.Abc = Abc;
+    }
 
     public SalariedEmployee() {
     }
 
-    public SalariedEmployee(double weeklySalary) {
-        this.weeklySalary = weeklySalary;
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAbc() {
+        return Abc;
+    }
+
+    public void setAbc(String Abc) {
+        this.Abc = Abc;
+    }
+
+   
+    
 
     public double getWeeklySalary() {
         return weeklySalary;
@@ -29,8 +62,10 @@ public class SalariedEmployee implements Payable{
 
     @Override
     public String toString() {
-        return "SalariedEmployee{" + "weeklySalary=" + weeklySalary + '}';
+        return "SalariedEmployee{" + "weeklySalary=" + weeklySalary + ", name=" + name + ", lastname=" + lastname + ", Abc=" + Abc + '}';
     }
+
+    
     
     @Override
     public double getPaymentAmount()
